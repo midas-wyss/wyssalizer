a7dd67032b3b48477db6138915dfe7f6706c85cb
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-11-14 09:30:12 -0500

    simplifying R scripts to be more contained. broke plotting scripts into 3 different files for easier debugging/adding.

--- R/network_animations.R
+++ R/network_animations.R
@@ -1,15 +1,5 @@
 # PLOTTING
 
-# plot papers over the years for faculty
-# p1 <- paper_totals %>% 
-#   dplyr::filter(., type == "core") %>%
-#   ggplot() + 
-#   geom_point(aes(x = as.factor(year), y = faculty, size = total_papers))
-# ggplotly(p1)
-
-
-
-
 ##### GIFS! #####
 # gganimate bit for everyone
 # first organize the data
@@ -207,37 +197,6 @@ igraph::graph_from_data_frame(d = N,
 gganimate::anim_save(filename = paste0("res/",format(Sys.Date(),"%Y-%m-%d"), "_coauthorship-whitebg.gif"))
 
 
-# patents
-all_papers %>% 
-  dplyr::filter(., year >= 2006, journal != "") %>% 
-  dplyr::filter(., grepl("US Patent", tofilter, ignore.case = TRUE)) %>%
-  dplyr::filter(., !grepl("Supplement", tofilter, ignore.case = TRUE)) %>%
-  dplyr::filter(., !grepl("Conference", tofilter, ignore.case = TRUE)) %>%
-  dplyr::mutate(., journal = tolower(journal)) %>% 
-  dplyr::mutate(., journal = "patents/patent applications") %>%
-  dplyr::group_by(.,journal, year) %>% 
-  dplyr::count() %>% 
-  ggplot() + 
-  geom_col(aes(x = as.factor(year), y = n), color = "black", fill = "orange") + 
-  # ylim(c(0, 1000)) + 
-  labs(title = "Patent explosion at the Wyss") +
-  theme_minimal() + 
-  theme(axis.title = element_blank(),
-        # panel.background = element_rect(fill = "black", color = "black"),
-        # plot.background = element_rect(fill = "black"),
-        axis.text = element_text(color = "black", size = 14),
-        axis.text.x = element_text(angle = 45, vjust = 0.7, hjust = 0.8),
-        axis.line = element_line(color = "black"),
-        axis.ticks = element_line(color = "black"),
-        axis.ticks.length = unit(2, "mm"),
-        panel.grid = element_blank(),
-        legend.position = "none") +
-  gganimate::transition_reveal(year)
-gganimate::anim_save(filename = paste0("res/gifs/",format(Sys.Date(),"%Y-%m-%d"), "_animation-patent-explosion-whitebg.gif"))
-ggsave(filename = paste0("res/img/", format(Sys.Date(), "%Y-%m-%d"), "_patents-per-year.pdf"),
-       height = 8,
-       width = 11,
-       dpi = 600)
-  
+
   
 
